20812bc5285b86842c70dec03474385460fa3316
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2023-08-08 12:42:28 -0700

    Handle possible errors on loading with unloaded sounds.

--- Source/ErrorsScreamAtYou.cs
+++ Source/ErrorsScreamAtYou.cs
@@ -16,10 +16,10 @@ namespace ErrorsScreamAtYou
 	}
 
 
-	[HarmonyPatch(typeof(Log), nameof(Log.Error), new Type[] { typeof(string)})]
+	[HarmonyPatch(typeof(Verse.Log), nameof(Verse.Log.Error), new Type[] { typeof(string)})]
 	public static class ScreamAtYou
 	{
-		// Not a defof so this works while loading before DefOfs are bound
+		// Not a defof so this might work while loading before DefOfs are bound
 		public static SoundDef _TD_Scream;
 		public static SoundDef TD_Scream
 		{
@@ -31,9 +31,84 @@ namespace ErrorsScreamAtYou
 				return _TD_Scream;
 			}
 		}
-		public static void Postfix()
+
+		private static string _text;
+		private static int _tick;
+		public static bool needsErrorScream;
+		public static void Postfix(string text)
 		{
-			TD_Scream?.PlayOneShotOnCamera();
+			try
+			{
+				// catch recursive errors
+				if (text == _text && (Current.Game?.tickManager?.TicksAbs ?? _tick) == _tick)
+				{
+					Log.Message($"TD_Scream skipping Error {text}");
+					return;
+				}
+				_tick = Current.Game?.tickManager?.TicksAbs ?? 0;
+				_text = text;
+
+
+				if (!StaticConstructorOnStartupUtility.coreStaticAssetsLoaded)
+				{
+					Log.Message($"Needs Error Scream later (!StaticConstructorOnStartupUtility.coreStaticAssetsLoaded)");
+					needsErrorScream = true;
+					return;
+				}
+
+
+				if (TD_Scream == null)
+				{
+					Log.Message($"Needs Error Scream later (TD_Scream == null)");
+					needsErrorScream = true;
+					return;
+				}
+
+
+				if (!TD_Scream.subSounds.Any(ss=> ss.Duration.max > 0))
+				{
+					Log.Message($"Needs Error Scream later (subSounds duration 0)");
+					needsErrorScream = true;
+					return;
+				}
+
+
+
+				// Okay we should be good.
+				Log.Message($"Playing TD_Scream ({TD_Scream})");
+				TD_Scream?.PlayOneShotOnCamera();
+
+
+				if (!Find.SoundRoot.oneShotManager.PlayingOneShots.Any(o => o?.subDef?.parentDef == TD_Scream))
+				{
+					Log.Message($"Needs Error Scream later (not playing)");
+					needsErrorScream = true;
+				}
+			}
+			catch(Exception e)
+			{
+				Log.Message($"Needs Error Scream later (exception {e})");
+				needsErrorScream = true;
+			}
+		}
+	}
+
+	[StaticConstructorOnStartup]
+	public static class ErrorOnStartup
+	{
+
+		static ErrorOnStartup()
+		{
+			Log.Message($"ErrorOnStartup: {ScreamAtYou.needsErrorScream}");
+			LongEventHandler.QueueLongEvent(delegate
+			{
+				Log.Message($"Doing Error Scream later: {ScreamAtYou.needsErrorScream}");
+				if (ScreamAtYou.needsErrorScream)
+				{
+					Log.Message($"Playing TD_Scream (later, TD_Scream = {ScreamAtYou.TD_Scream})");
+					ScreamAtYou.TD_Scream?.PlayOneShotOnCamera();
+				}
+			}, null, false, null);
 		}
 	}
 }
